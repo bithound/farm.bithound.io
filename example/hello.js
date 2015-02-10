6abc299804f79faca1e6c24137d2ac6dd1c6a336
Gord Tanner <gord@bithound.io>
2015-02-09 16:35:51 -0800

    Lets get ready to actually open source this
    - added the LICENSE
    - filled out the readme
    - added an example
    - updated the calls to not require a config (to make it simple).
    - bump version

---
+++ example/hello.js
@@ -0,0 +1,37 @@
+var farm = require('../lib/farm'),
+    cluster = require('cluster');
+
+// We are using cluster to keep this all in one file
+if (cluster.isMaster) {
+  //We always need a broker running (on localhost)
+  var broker = require('../lib/broker').start();
+
+  //join the farm (started via the broker)
+  farm.join('localhost');
+
+  //start a couple child processes (which will be our workers)
+  cluster.fork();
+  cluster.fork();
+  cluster.fork();
+
+  farm.jobs.send('hello', function (err, world) {
+    farm.jobs.distribute(['?', '?', '?'], function (err, excited) {
+      console.log('hello', world, excited.join(''));
+      broker.stop();
+      process.exit();
+    });
+  });
+}
+else {
+  // setup a worker callback function
+  farm.worker(function (task, callback) {
+    switch (task) {
+      case 'hello': return callback(null, 'world');
+      case '?': return callback(null, '!');
+      default: return callback(new Error('huh?'));
+    }
+  });
+
+  //connect to localhost and designate we are a worker
+  farm.join('localhost', {worker: true});
+}
